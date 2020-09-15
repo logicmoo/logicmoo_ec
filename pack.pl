f272ac0dcb835ce07dbf16b18bc5812d8f037b4e
logicmoo <logicmoo@gmail.com>
2020-09-15 16:12:33 -0700

    version_bump

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(planner_api).
 title('planner_api -- A SWI-Prolog Pack that lets Prolog code seamlessly use planners speaking PDDLish and OCLh').
-version('1.1.118').
+version('2.0.1').
 author( 'Andrew Dougherty', 'andrewdo@frdcsa.org' ).
 author( 'Douglas Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
