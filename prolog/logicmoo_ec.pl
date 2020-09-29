a4124fbdc6ec384d5eb30d3b5f39e23d5e5c0858
Douglas R. Miles <logicmoo@gmail.com>
2020-09-29 02:39:23 -0700

    prolog/logicmoo_flp.pl

--- prolog/logicmoo_ec.pl
+++ prolog/logicmoo_ec.pl
@@ -1,10 +1,10 @@
+:- module(logicmoo_ec,[]).
 
+:- reexport(logicmoo_planner).
+:- use_module(library(lps_corner)).
+:- reexport(library(ec_planner/ec_lps_convert)).
+:- reexport(library(ec_planner/ec_reader)).
 
-
-:- use_module(library(logicmoo_webui)).
 :- use_module(library(eggdrop)).
 :- egg_go.
-:- use_module(library(lps_corner)).
-:- use_module(library(ec_planner/ec_lps_convert)).
-:- use_module(library(ec_planner/ec_reader)).
 
