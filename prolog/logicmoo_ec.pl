bf95a5c028aa2469bf256e1d50f0647beb5f86a9
Douglas R. Miles <logicmoo@gmail.com>
2020-09-30 05:06:36 -0700

    fixed transform

--- prolog/logicmoo_ec.pl
+++ prolog/logicmoo_ec.pl
@@ -1,7 +1,57 @@
 :- module(logicmoo_ec,[]).
 
+% [Required] Load the Logicmoo Library Utils
+:- ensure_loaded(library(logicmoo_utils)).
+
 :- reexport(logicmoo_planner).
-:- use_module(library(lps_corner)).
+
+:- if(\+ exists_source(swish(lib/render))).
+:- add_absolute_search_folder(swish,'../../logicmoo_webui/swish').
+:- endif.
+
+
+/*
+:- add_absolute_search_folder(pack,'../ext/').
+:- initialization(attach_packs).
+:- initialization(attach_packs,now).
+
+:- if( \+ exists_source(library(lps_corner))).
+:- initialization(attach_packs).
+:- initialization(attach_packs,now).
+:- add_absolute_search_folder(pack,'../../logicmoo_webui/').
+:- endif.
+
+:- initialization(attach_packs).
+:- initialization(attach_packs,now).
+*/
+
+
+swish:is_really_module.
+
+run_lps_corner1(File):- with_abs_paths(run_lps_corner_file1,File).
+
+system:run_lps_corner_file1(File):- wdmsg(run_lps_corner_file1(File)),fail.
+system:run_lps_corner_file1(File):-
+   absolute_file_name(File,DB),
+   DB:use_module(library(lps_corner)), 
+   %listing(db:actions/1),
+   %listing(interpreter:actions/1),
+   interpreter:check_lps_program_module(DB),
+   interpreter:must_lps_program_module(DB),
+   DB:consult(DB),
+      interpreter:must_lps_program_module(DB),
+   writeq(listing(DB:_)),
+   elsewhere:listing(DB:_),
+   DB:golps(X),
+   %listing(interpreter:lps_program_module/1),
+   wdmsg(dB(DB,X)).
+
+%load_lps_corner:-!.
+user:test_lps_corner:- 
+  run_lps_corner1(library('../examples/binaryChop2.pl')).
+     
+
+%:- use_module(library(lps_corner)).
 :- reexport(library(ec_planner/ec_lps_convert)).
 :- reexport(library(ec_planner/ec_reader)).
 
--- prolog/logicmoo_planner.pl
+++ prolog/logicmoo_planner.pl
@@ -4,29 +4,22 @@
 :- style_check(-singleton).
 
 :- use_module(library(prolog_pack)).
+
 :- if( \+ prolog_pack:current_pack(logicmoo_ec)).
-:- dynamic   user:file_search_path/2.
-:- multifile user:file_search_path/2.
-:- prolog_load_context(directory,Dir),
-   DirFor = logicmoo_ec,
-   (( \+ user:file_search_path(DirFor,Dir)) ->asserta(user:file_search_path(DirFor,Dir));true),
-   absolute_file_name('../..',Y,[relative_to(Dir),file_type(directory)]),
-   (( \+ user:file_search_path(pack,Y)) ->asserta(user:file_search_path(pack,Y));true).
+:- add_absolute_search_folder(pack,'../../').
 :- attach_packs.
 :- initialization(attach_packs).
 :- endif.
 
+:-  add_absolute_search_folder(ext,('../ext/')).
+% :-  add_absolute_search_folder(ext,library('../ext')).
 
 :- if( \+  user:file_search_path(pddl,_) ).
-:- prolog_load_context(directory,Dir),
-   must((absolute_file_name('../test/pddl_tests/',Y,[relative_to(Dir),file_type(directory)]),
-   asserta(user:file_search_path(pddl,Y)))),
-   
-   must((absolute_file_name('../test/uw-yale-pddl/strict-domains/',Y,[relative_to(Dir),file_type(directory)]),
-   asserta(user:file_search_path(pddl,Y)))),
-   
-   must((absolute_file_name('../test/uw-yale-pddl/domains/',Y,[relative_to(Dir),file_type(directory)]),
-   asserta(user:file_search_path(pddl,Y)))).
+
+:-  add_absolute_search_folder(pddl,'../test/pddl_tests/'),
+    add_absolute_search_folder(pddl,'../test/uw-yale-pddl/strict-domains/'),
+    add_absolute_search_folder(pddl,'../test/uw-yale-pddl/domains/'),
+    !.
          
 :- endif.
 
